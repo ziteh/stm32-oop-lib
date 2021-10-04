ef10cf8a04a92cc9f0224bd1f841adb1ac054a2f
ziteh <honmonoh@gmail.com>
2021-10-04 09:11:59 +0800

    Add f103rb_lib.hpp

---
+++ lib/f103rb_lib.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file    f103rb_lib.hpp
+ * @author  ZiTe <honmonoh@gmail.com>
+ * @brief   Include all F103RB libraries.
+ */
+
+#ifndef F103RB_LIB_HPP_
+#define F103RB_LIB_HPP_
+
+#include "f103rb_gpio.hpp"
+#include "f103rb_pwm.hpp"
+#include "f103rb_usart.hpp"
+
+#endif /* F103RB_LIB_HPP_ */
